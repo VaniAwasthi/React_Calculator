b11e39b4ac63c979fa2dd66af4233250b63eab6a
Vani Awasthi <awasthivani95@gmail.com>
2023-04-17 18:05:30 +0530

    last change to calculator

--- src/App.js
+++ src/App.js
@@ -25,7 +25,6 @@ const App = () => {
     setResult("");
   };
 
-  const buttonColor = "#f2a33c";
 
   return (
     <div className="App">
@@ -36,25 +35,25 @@ const App = () => {
           <Button symbol="7" handleClick={addToText} />
           <Button symbol="8" handleClick={addToText} />
           <Button symbol="9" handleClick={addToText} />
-          <Button symbol="/" color={buttonColor} handleClick={addToText} />
+          <Button symbol="/"  handleClick={addToText} />
         </div>
         <div className="row">
           <Button symbol="4" handleClick={addToText} />
           <Button symbol="5" handleClick={addToText} />
           <Button symbol="6" handleClick={addToText} />
-          <Button symbol="*" color={buttonColor} handleClick={addToText} />
+          <Button symbol="*"  handleClick={addToText} />
         </div>
         <div className="row">
           <Button symbol="1" handleClick={addToText} />
           <Button symbol="2" handleClick={addToText} />
           <Button symbol="3" handleClick={addToText} />
-          <Button symbol="+" color={buttonColor} handleClick={addToText} />
+          <Button symbol="+"  handleClick={addToText} />
         </div>
         <div className="row">
           <Button symbol="0" handleClick={addToText} />
           <Button symbol="." handleClick={addToText} />
           <Button symbol="=" handleClick={calculateResult} />
-          <Button symbol="-" color={buttonColor} handleClick={addToText} />
+          <Button symbol="-"  handleClick={addToText} />
         </div>
         <Button symbol="Clear" color="red" handleClick={resetInput} />
       </div>
